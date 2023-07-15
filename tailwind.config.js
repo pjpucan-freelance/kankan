c50edce32bf2565410b5a36c7142f6302bcf00b8
pjpucan-freelance <139597689+pjpucan-freelance@users.noreply.github.com>
2023-07-15 22:50:26 +0800

    tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/*.{html,js}",
+    "./layout/*.liquid",
+    "./snippets/*.liquid",
+    "./sections/*.liquid"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
